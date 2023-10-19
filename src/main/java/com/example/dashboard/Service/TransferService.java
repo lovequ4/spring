833db099f9396ecba25a9d04c87c153f4af7b58c
sam <a0983131803@gmail.com>
2023-10-19 08:40:55 +0800

    add cancel transfer

--- src/main/java/com/example/dashboard/Service/TransferService.java
+++ src/main/java/com/example/dashboard/Service/TransferService.java
@@ -1,9 +1,12 @@
 package com.example.dashboard.Service;
 
+import java.util.UUID;
+
 import org.springframework.http.ResponseEntity;
 
 import com.example.dashboard.Entity.Transfer;
 
 public interface TransferService  {
     ResponseEntity<String> createTransfer(Transfer transfer);
+    ResponseEntity<String> cancelTransfer(UUID Id);
 }
