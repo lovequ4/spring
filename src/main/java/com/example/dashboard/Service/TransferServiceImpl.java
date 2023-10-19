978f5dd70fe5a0600b93b996c5a105cb70878e8e
sam <a0983131803@gmail.com>
2023-10-19 08:42:04 +0800

    add cancel transfer &&  fix processingTransfer func to void

--- src/main/java/com/example/dashboard/Service/TransferServiceImpl.java
+++ src/main/java/com/example/dashboard/Service/TransferServiceImpl.java
@@ -3,10 +3,11 @@ package com.example.dashboard.Service;
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.Date;
+import java.util.UUID;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -20,16 +21,19 @@ import com.example.dashboard.Repository.TransferRepository;
 
 @Service
 public class TransferServiceImpl implements TransferService{
+    
     private TransferRepository transferRepository;
     private  AccountRepository accountRepository;
-    private ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
     
+    private ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+    private ScheduledFuture<?> scheduledTask;
+
     public TransferServiceImpl(AccountRepository accountRepository,TransferRepository transferRepository){
         this.accountRepository = accountRepository;
         this.transferRepository = transferRepository;
     }
 
-   
+    //建立轉帳
     public ResponseEntity<String> createTransfer(Transfer transfer){
         
         //檢查 收款人存在
@@ -46,16 +50,31 @@ public class TransferServiceImpl implements TransferService{
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Sender not found");
         }
 
-        //取得匯款人金額
+        //取得匯款人帳戶金額
         BigDecimal senderAmount = senderAccount.getAmount();
 
         //取得匯款金額
         BigDecimal transferAmount = transfer.getTransferAmount();
 
-        
-        if (transferAmount.compareTo(senderAmount) > 0) {
+        //如果帳戶金額小於匯款或是等於0，交易狀態為E
+        if (transferAmount.compareTo(senderAmount) > 0 ) {
+
             transfer.setStatus("E"); //E == error
+            transfer.setCreatedTime(Date.from(Instant.now()));
+            transfer.setEndTime(Date.from(Instant.now()));
+            transferRepository.save(transfer);
+
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Insufficient funds");
+            
+        }else if (transferAmount.compareTo(BigDecimal.ZERO) == 0) {
+            transfer.setStatus("E"); //E == error
+
+            transfer.setCreatedTime(Date.from(Instant.now()));
+            transfer.setEndTime(Date.from(Instant.now()));
+            transferRepository.save(transfer);
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("TransferAmount Cannot be zero.");
+        
         }else{
            
             transfer.setStatus("P"); // P == Pending
@@ -64,27 +83,18 @@ public class TransferServiceImpl implements TransferService{
 
             transferRepository.save(transfer);
 
-            // 使用AtomicReference包裝response可讓您在Lambda表達式內部修改其值，
-            // 解決"Local variable initialResponse defined in an enclosing scope must be final or effectively final"的問題。
-            AtomicReference<ResponseEntity<String>> responseRef = new AtomicReference<>(ResponseEntity.ok("Transfer is pending and can be canceled within 5 minutes."));
-    
-            executorService.schedule(() -> {
 
-                ResponseEntity<String> processingResponse = processingTransfer(transfer);
-                
-                if (processingResponse != null) {
-                    responseRef.set(processingResponse);
-                }
+            scheduledTask = scheduler.schedule(() -> {
+                processingTransfer(transfer);
+            }, 1, TimeUnit.MINUTES);
 
-            }, 5, TimeUnit.MINUTES);
-
-            return responseRef.get();
+            return ResponseEntity.status(HttpStatus.OK).body("Transfer is pending and can be canceled within 5 minutes.");
 
         }
     }
 
-    
-    private ResponseEntity<String> processingTransfer(Transfer transfer) {
+    //處理轉帳
+    private void processingTransfer(Transfer transfer) {
 
         //檢查 收款人存在
         Account recipientAccount = accountRepository.findById(transfer.getRecipientAccount().getId()).orElse(null);
@@ -120,8 +130,38 @@ public class TransferServiceImpl implements TransferService{
         accountRepository.save(recipientAccount);
 
         transferRepository.save(transfer);
-
-        return ResponseEntity.ok("Transfer Successfully.");
     }
     
+
+    
+    //取消轉帳
+    public ResponseEntity<String> cancelTransfer(UUID Id){
+
+        Transfer transfer = transferRepository.findByTransferId(Id);
+
+        
+        if(transfer == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("TransferRecord Not found.");
+        }
+
+        //判斷交易狀態是P等待，才執行
+        if(transfer.getStatus().equals("P")){
+
+           
+            if (scheduledTask != null) {
+                scheduledTask.cancel(true); // 取消任务，不中断正在运行的任务
+            }
+
+            transfer.setStatus("C"); // C == cancel
+            transfer.setCreatedTime(Date.from(Instant.now()));
+            transfer.setEndTime(Date.from(Instant.now()));
+            transferRepository.save(transfer);
+
+            return ResponseEntity.status(HttpStatus.OK).body("Transfer Canceled Successfully."); 
+        
+        }else{
+           
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The transfer has been processed and cannot be canceled.");
+        }    
+    }
 }
