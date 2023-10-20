299510f7089911390f9159a57d34fc174eab6368
sam <a0983131803@gmail.com>
2023-10-20 09:32:48 +0800

    add findBySenderAccountId

--- src/main/java/com/example/dashboard/Repository/TransferRepository.java
+++ src/main/java/com/example/dashboard/Repository/TransferRepository.java
@@ -1,11 +1,15 @@
 package com.example.dashboard.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.dashboard.Entity.Transfer;
 
+
+
 public interface TransferRepository extends JpaRepository<Transfer,Long>{
     Transfer findByTransferId(UUID Id);
+    List<Transfer> findBySenderAccountId(long Id);
 }
