6b48dbc5db4eaca9d430400e35808ffe6c33dee4
sam <a0983131803@gmail.com>
2023-10-20 09:35:15 +0800

    update

--- src/main/java/com/example/dashboard/Security/SecurityConfig.java
+++ src/main/java/com/example/dashboard/Security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.dashboard.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,16 +21,9 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-            .requestMatchers(
-                "/api/**",
-                "/api/users/signup",
-                "/api/users/login",
-                "/swagger-ui/**",
-                "/v3/api-docs/**"
-                )
-                .permitAll()
-                .anyRequest()
-                .authenticated()
+                .requestMatchers(  "/api/**","/api/accounts/signup","/api/accounts/signin","/swagger-ui/**","/v3/api-docs/**").permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/accounts/{Id}","/api/transfer").permitAll()
+                .anyRequest().authenticated()
             );
             
         return http.build();  
